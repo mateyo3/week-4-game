1cec894d4b8b5178756f8e8d47514b8a40c42788
mateyo3 <mateoreyes720@gmail.com>
2017-09-06 21:59:29 -0700

    psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+var winCount = 0;
+	lossCount = 0;
+	yourNumber 0;
+	hostNumber = 0;
+
+//host name array
+var hostArray = ["Minato", "Kakashi", "Naruto", "Sasuke", "Sakura"]
+
+
+//computer randomly picks host
+//display host name in HTML .hostName
+
+//set host name === host image
+//if host name is Minato, then image is minato.png
+//if host name is Kakashi, then image is kakashi.png
+//if host name is Naruto, then image is naruto.png
+//if host name is Sasuke, then image is sasuke.png
+//if host name is Sakura, then image is sakura.png
+
+//diplay host image in HTML .hostImage
+
+
+//randomly generate hostNumber between 50-125
+//display hostNumber in HTML.hostNumber
+
+
+//randomly assign value to mask 1, 2 through 20
+//onclick function to image
+//onclick adds value to yourNumber
+
+
+//randomly assign value to mask 2, 2 through 20
+//onclick function to image
+//onclick adds value to yourNumber
+
+
+//randomly assign value to mask 3, 2 through 20
+//onclick function to image
+//onclick adds value to yourNumber
+
+
+//randomly assign value to mask 4, 2 through 20
+//onclick function to image
+//onclick adds value to yourNumber
+
+
+//if yourNumber === hostNumber, then winCount ++
+//reset game
+
+
+//if yourNumber > hostNumber, then lossCount ++
+//reset game
+
+
+//display yourNumber total in HTML
+
+
+
+
