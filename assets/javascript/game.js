9ea238ea5417050c550b0e394cd29f6bc515b36a
mateyo3 <mateoreyes720@gmail.com>
2017-09-10 00:05:24 -0700

    host image and name change

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
-var winCount = 0;
-	lossCount = 0;
+var winCount = null;
+	lossCount = null;
 	playerTotal = 0;
-	hostNumber = 0;
+	hostNumber = null;
 
 //host name array
 var hostArray = ["Minato", "Kakashi", "Naruto", "Sasuke", "Sakura"];
@@ -25,37 +25,66 @@ $(document).ready(function() {
 	// document.querySelector(".hostName").innerHTML = host;
 // set host name === host image
 // if host name is Minato, then image is minato.png
-	if (host === "Sakura") {
-		$("#hostImage").show("assets/image/sakura1.png")
-
+	if (host === hostArray[0]) {
+		$(".hostImage").attr("src", "assets/images/minato.png")
+		$(".host-name").text(hostArray[0])
 	}
-//if host name is Kakashi, then image is kakashi1.png
-//if host name is Naruto, then image is naruto1.png
-//if host name is Sasuke, then image is sasuke1.png
-//if host name is Sakura, then image is sakura1.png
+	//if host name is Kakashi, then image is kakashi1.png
+	if (host === hostArray[1]) {
+		$(".hostImage").attr("src", "assets/images/kakashi1.png")
+		$(".host-name").text(hostArray[1])
+	}
+	//if host name is Naruto, then image is naruto1.png
+	if (host === hostArray[2]) {
+		$(".hostImage").attr("src", "assets/images/naruto1.png")
+		$(".host-name").text(hostArray[2])
+	}
+	//if host name is Sasuke, then image is sasuke1.png
+	if (host === hostArray[3]) {
+	$(".hostImage").attr("src", "assets/images/sasuke1.png")
+	$(".host-name").text(hostArray[3])
+	}
+	//if host name is Sakura, then image is sakura1.png
+	if (host === hostArray[4]) {
+	$(".hostImage").attr("src", "assets/images/sakura1.png")
+	$(".host-name").text(hostArray[4])
+	}
+
+
+
+
 
 //diplay host image in HTML .hostImage
 
 
 //randomly generate hostNumber between 50-125
-	var random = Math.floor(Math.random() *125) + 5;
+	var random = Math.floor(Math.random() *125) + 50;
 //display hostNumber in HTML.hostNumber
-	$("#hostNumber").html(random);
+	$(".hostNumber").text(random);
 		console.log(random);
 
 
-//randomly assign value to mask 1, 2 through 20
+//randomly assign value to masks, 1 through 12
+	var maskOneValue = Math.floor(Math.random() *12) + 2;
+		console.log("mask1 value: " + maskOneValue);
+
+	var maskTwoValue = Math.floor(Math.random() *12) + 2;
+		console.log("mask2 value: " + maskTwoValue);
+
+	var maskThreeValue = Math.floor(Math.random() *12) + 2;
+		console.log("mask3 value: " + maskThreeValue);
+
+	var maskFourValue = Math.floor(Math.random() *12) + 2;
+		console.log("mask4 value: " + maskFourValue);
+
 //onclick function to mask1
 	$("#mask1").on("click", function() {
   		console.log("mask1 clicked");
 		//onclick adds value to yourNumber
-  		var maskOneValue = Math.floor(Math.random() *12) + 1;
-  		  	console.log(maskOneValue);
-
-    var maskOne = ($(this).attr("data-maskOneValue"));
-    userTotal = parseInt(maskOne);
+    var number = ($(this).attr("data-number"));
+    userTotal = parseInt(number);
     	console.log(userTotal);
-    playerTotal += userTotal;
+    userTotal += playerTotal;
     	console.log(playerTotal);
 
     if (userTotal === hostNumber){
@@ -71,12 +100,11 @@ $(document).ready(function() {
 
 
 //randomly assign value to mask 2, 2 through 20
+
 //onclick function to mask2
 	$("#mask2").on("click", function() {
   		console.log("mask2 clicked");
   		//onclick adds value to yourNumber
-  		var maskTwoValue = Math.floor(Math.random() *12) + 1;
-  			console.log(maskTwoValue);
   		yourNumber++;
   		console.log(yourNumber);
 	});
@@ -88,8 +116,6 @@ $(document).ready(function() {
 	$("#mask3").on("click", function() {
   		console.log("mask3 clicked");
   		//onclick adds value to yourNumber
-  		var maskThreeValue = Math.floor(Math.random() *12) + 1;
-  		  	console.log(maskThreeValue);
   		yourNumber++;
   		console.log(yourNumber);
 	});
@@ -101,8 +127,6 @@ $(document).ready(function() {
 	$("#mask4").on("click", function() {
  		 console.log("mask4 clicked");
  		 //onclick adds value to yourNumber
-  		var maskFourValue = Math.floor(Math.random() *12) + 1;
-  			  console.log(maskFourValue);
   		yourNumber++;
   		console.log(yourNumber);
 	});
